f6db552dfb4c5a04fb3b84cdd1e159ce8caa39af
VISHAAL-KUMAR-A <107105545+VISHAAL-KUMAR-A@users.noreply.github.com>
2024-10-07 14:12:26 +0530

    Update serializer.py

--- jobs/serializer.py
+++ jobs/serializer.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
-from .models import UserProfile, Job
-from django.contrib.auth.models import User
+from .models import UserProfile, UserPreferences, Job
+
+class UserPreferencesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserPreferences
+        exclude = ('user',)
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    preferences = UserPreferencesSerializer()
+    
     class Meta:
         model = UserProfile
-        fields = ['skills', 'experience_level', 'desired_roles', 
-                 'preferred_locations', 'job_type']
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        preferences_data = validated_data.pop('preferences')
+        user_profile = UserProfile.objects.create(**validated_data)
+        UserPreferences.objects.create(user=user_profile, **preferences_data)
+        return user_profile
 
 class JobSerializer(serializers.ModelSerializer):
+    match_score = serializers.FloatField(read_only=True, required=False)
+    
     class Meta:
         model = Job
         fields = '__all__'
